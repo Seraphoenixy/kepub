6ec8a77f3cd20cfa99bbd542495467fca1ee4638
kaiser <1244713586@qq.com>
2021-06-12 18:18:24 +0800

    2021.6.12

--- src/trans.cpp
+++ src/trans.cpp
@@ -48,6 +48,7 @@ void custom_trans(icu::UnicodeString &str) {
   str.findAndReplace("~", "～");
   str.findAndReplace("･", "・");
   str.findAndReplace("ｰ", "ー");
+  str.findAndReplace("￫", "→");
 
   str.findAndReplace("妳", "你");
   str.findAndReplace("壊", "坏");
