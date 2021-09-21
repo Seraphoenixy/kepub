f4ef392a80eaa23059136aee3b8faec8887e5d5e
kaiser <1244713586@qq.com>
2021-09-21 14:51:59 +0800

    2021.9.21

--- script/build.sh
+++ script/build.sh
@@ -33,7 +33,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
       -o klib.deb
     sudo dpkg -i klib.deb
 
-  curl -L https://github.com/KaiserLancelot/kpkg/releases/download/v0.3.13/kpkg-v0.3.13-ubuntu-20.04.deb \
+  curl -L https://github.com/KaiserLancelot/kpkg/releases/download/v0.3.12/kpkg-v0.3.12-ubuntu-20.04.deb \
       -o kpkg.deb
   sudo dpkg -i kpkg.deb
 
