e82ed5bdd6ce7b1d0a839686de9a3227a9c2b209
kaiser <1244713586@qq.com>
2021-10-29 16:36:05 +0800

    2021.10.29

--- script/build.sh
+++ script/build.sh
@@ -29,7 +29,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo update-alternatives --install /usr/bin/llvm-cov llvm-cov /usr/bin/llvm-cov-13 400
   sudo update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-13 400
 
-  curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.7.5/klib-v0.7.5-ubuntu-20.04.deb \
+  curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.7.6/klib-v0.7.6-ubuntu-20.04.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
 
