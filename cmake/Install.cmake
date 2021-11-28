3f840504476ba5ee6bba36e1e0ecd6a21b95e088
kaiser <1244713586@qq.com>
2021-11-28 19:57:04 +0800

    2021.11.28

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -58,15 +58,17 @@ set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF)
 set(CPACK_INSTALL_CMAKE_PROJECTS ${KEPUB_BINARY_DIR} ${PROJECT_NAME} ALL .)
 
 # https://cmake.org/cmake/help/latest/cpack_gen/deb.html
-set(CPACK_PACKAGE_CONTACT "kaiser <KaiserLancelot123@gmail.com>")
+set(CPACK_PACKAGE_CONTACT "Kaiser <KaiserLancelot123@gmail.com>")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
     "Crawl novels from esjzone, ciweimao and sfacg. Generate epub from txt file"
 )
 set(CPACK_PACKAGE_VERSION
     ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
 
-# https://cmake.org/cmake/help/latest/manual/cpack-generators.7.html
-set(CPACK_GENERATOR "TGZ;DEB")
+# https://cmake.org/cmake/help/latest/cpack_gen/archive.html
+# https://cmake.org/cmake/help/latest/cpack_gen/deb.html
+set(CPACK_GENERATOR "TZST;DEB")
+set(CPACK_DEBIAN_COMPRESSION_TYPE "zstd")
 
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 
