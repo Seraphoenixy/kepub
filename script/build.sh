8ed6b2e78550486841a31e8d4b136a90ff5adf18
kaiser <1244713586@qq.com>
2021-12-18 19:20:37 +0800

    2021.12.18

--- script/build.sh
+++ script/build.sh
@@ -6,6 +6,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.10.8/klib-0.10.8-Linux.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
+
+  curl -L https://github.com/KaiserLancelot/kpkg/releases/download/v0.10.1/pyftsubset -o pyftsubset
+  mv pyftsubset /usr/local/bin/pyftsubset
+  chmod 755 /usr/local/bin/pyftsubset
 else
   echo "The system does not support: $OSTYPE"
   exit 1
