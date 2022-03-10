a2b8d5653287b3a215298c8add060890ced34865
kaiser <1244713586@qq.com>
2022-03-10 17:56:14 +0800

    use terminal_size()

--- src/progress_bar.cpp
+++ src/progress_bar.cpp
@@ -1,13 +1,16 @@
 #include "progress_bar.h"
 
+#include <klib/util.h>
+
 namespace kepub {
 
 using namespace indicators;
 
 ProgressBar::ProgressBar(const std::string &postfix_text,
                          std::int32_t max_progress)
-    : bar_(option::BarWidth(35), option::Start("["), option::Fill("="),
-           option::Lead(">"), option::Remainder(" "), option::End("]"),
+    : bar_(option::BarWidth(klib::terminal_size().first / 3),
+           option::Start("["), option::Fill("="), option::Lead(">"),
+           option::Remainder(" "), option::End("]"),
            option::PostfixText(postfix_text),
            option::ForegroundColor(Color::green),
            option::FontStyles(std::vector<FontStyle>{FontStyle::bold}),
