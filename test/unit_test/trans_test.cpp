5ee00b479e64e8c749033923c32e29b4d8c72581
Kaiser <1244713586@qq.com>
2022-03-17 20:11:39 +0800

    Add some full-width to half-width conversion

--- test/unit_test/trans_test.cpp
+++ test/unit_test/trans_test.cpp
@@ -3,6 +3,8 @@
 #include "trans.h"
 
 TEST_CASE("trans_str", "[trans]") {
+  CHECK(kepub::trans_str("Ｑ０", false) == "Q0");
+
   CHECK(kepub::trans_str("“安装后”", false) == "“安装后”");
   CHECK(kepub::trans_str("&amp;", false) == "&");
   CHECK(kepub::trans_str("安裝後?", true) == "安装后？");
