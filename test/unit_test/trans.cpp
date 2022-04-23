4479f7981ea291a06bf17ae3366067d4360ef86f
Kaiser <kaiserlancelot123@gmail.com>
2022-04-23 20:44:08 +0800

    More trans

--- test/unit_test/trans.cpp
+++ test/unit_test/trans.cpp
@@ -4,10 +4,9 @@
 
 TEST_CASE("trans_str", "[trans]") {
   CHECK(kepub::trans_str("Ｑ０", false) == "Q0");
-
   CHECK(kepub::trans_str("“安装后”", false) == "“安装后”");
   CHECK(kepub::trans_str("&amp;", false) == "&");
   CHECK(kepub::trans_str("安裝後?", true) == "安装后？");
-
   CHECK(kepub::trans_str("，，，", false) == "，");
+  CHECK(kepub::trans_str("安　装", false) == "安 装");
 }
